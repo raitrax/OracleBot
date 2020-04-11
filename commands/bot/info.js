854ee53cf922210e7b32e11cf55336c75a2bc4db
raitrax <maxime.maurer@viacesi.fr>
2020-04-11 17:58:40 +0200

    Update info.js

--- commands/bot/info.js
+++ commands/bot/info.js
@@ -15,13 +15,16 @@ module.exports = class InviteCommand extends Command {
   }
 
   run (msg) {
+    var test = this.client.users.get("182517037795377153")
+    console.log(test.avatarURL());
     const infoEmbed = new MessageEmbed()
     .setTitle("Informations")
     .setDescription("Voici quelques informations concernant")
     .setColor(0xcd6e57)
-    .setFooter("Raitrax | Développeur C#", "https://www.draftman.fr/images/avatar.jpg")
+    //.setFooter("Raitrax | Développeur C#", "https://www.draftman.fr/images/avatar.jpg")
+    .setFooter("Raitrax | Développeur C#", test.avatarURL())
     .addField("Lib du bot", "[discord.js](https://discord.js.org)", true)
-    .addField("DiscordBots", "[discordbots.org/draftbot](https://discordbots.org/bot/318312854816161792)", true)
+    //.addField("DiscordBots", "[discordbots.org/draftbot](https://discordbots.org/bot/318312854816161792)", true)
     .addField("Développeur", "Raitrax#8360", true)
     .setTimestamp()
 
