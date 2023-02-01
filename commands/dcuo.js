70c2d082b27c23be545df266f5389d91828aca77
raitrax <maxime.maurer@viacesi.fr>
2023-02-02 00:39:34 +0100

    maj dcuo + multi serveur

---
+++ commands/dcuo.js
@@ -0,0 +1,91 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { request } = require('undici');
+const fs = require('fs');
+const { ownersID } = require('../config.json');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dcuo')
+        .setDescription('Recherche stat personnage DCUO')
+        .addStringOption(option =>
+            option.setName('nom')
+                .setDescription(`Nom du personnage`)
+                .setRequired(true)
+
+        ),
+    async execute(interaction) {
+        const nom = interaction.options.getString('nom');
+        await interaction.deferReply();
+
+        const characterResult = await request(`https://census.daybreakgames.com/s:raitrax/get/dcuo/character?name=*${nom}&c:case=false&c:limit=1&c:exactMatchFirst=true`);
+        const { character_list } = await characterResult.body.json();
+
+        console.log(character_list);
+        if (!character_list.length) {
+            return await interaction.editReply(`Aucun résultat trouvé pour le personnage : **${nom}**.`);
+        }
+        const [answer] = character_list;
+
+        let ligueName = "";
+        const guidRosterResult = await request(`https://census.daybreakgames.com/s:raitrax/get/dcuo/guild_roster?character_id=${answer.character_id}`);
+        const { guild_roster_list } = await guidRosterResult.body.json();
+        if (!guild_roster_list.length) {
+            ligueName = "Aucun";
+        }
+        const [answer2] = guild_roster_list;
+
+        const guidListResult = await request(`https://census.daybreakgames.com/s:raitrax/get/dcuo/guild?guild_id=${answer2.guild_id}`);
+        const { guild_list } = await guidListResult.body.json();
+        if (!guild_list.length) {
+            ligueName = "Aucun";
+        }
+        const [answer3] = guild_list;
+        ligueName = answer3.name;
+
+        let Resistance = `Santé : ${answer.max_health}\nPouvoir : ${answer.max_power}\nDéfense : ${answer.defense}\nEndurance : ${answer.toughness}`;
+        let Action = `Puissance : ${answer.might}\nPrécision : ${answer.precision}\nVitalisation : ${answer.vitalization}\nRestauration : ${answer.restoration}\nDominance : ${answer.dominance}`;
+
+        let deplacement = "";
+        switch (answer.movement_mode_id) {
+            case '3313':
+                deplacement = "Vol";
+                break;
+            case '3314':
+                deplacement = "Super vitesse";
+                break;
+            case '3315':
+                deplacement = "Planeur";
+                break;
+            case '3316':
+                deplacement = "Surf";
+                break;
+            default:
+                deplacement = "Non trouvé";
+                break;
+        }
+
+        let pouvoir = "";
+        switch (answer.power_type_id) {
+            case '1810455':
+                pouvoir = "Quantum";
+                break;
+            default:
+                pouvoir = "Non trouvé";
+                break;
+        }
+        const embed = new EmbedBuilder()
+            .setColor(0xEFFF00)
+            .setTitle(answer.name)
+            .addFields(
+                { name: 'CR', value: answer.combat_rating, inline: true },
+                { name: 'PVP', value: answer.pvp_combat_rating, inline: true },
+                { name: 'SP', value: answer.skill_points, inline: true },
+                { name: 'Pouvoir', value: pouvoir, inline: true },
+                { name: 'Déplacement', value: deplacement, inline: true },
+                { name: 'Ligue', value: ligueName, inline: true },
+                { name: 'Resistance', value: Resistance, inline: true },
+                { name: 'Action', value: Action, inline: true }
+            );
+
+        await interaction.editReply({ embeds: [embed] });
+    },
+};
--- deploy-commands.js
+++ deploy-commands.js
@@ -16,6 +16,25 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		/*const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);*/
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+
